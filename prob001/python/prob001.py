9fe465d990c472bc3e69d69c6da22793540a96a1
iku000888 <kanumaiku@gmail.com>
2015-03-26 00:59:21 +0900

    prob 1 in python

---
+++ prob001/python/prob001.py
@@ -0,0 +1,4 @@
+import sys#need this to read from stdin
+line = sys.stdin.read();#read one line of text
+line =line[::-1];#[begin:end:step] syntax that happens to reverse an array. Cool.
+print line;#print what I got
---
+++ prob001/python/test.sh
@@ -0,0 +1,6 @@
+
+
+#input string echo passed into the test program.
+echo 'stressed'|python prob001.py > prob0001.out
+echo 'here is the diff with the solution (blank means correct): '
+diff prob0001.out answer
