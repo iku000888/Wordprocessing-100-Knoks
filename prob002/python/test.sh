ffa65d929545e83467a57ea354ae7ea0f5c19a1e
iku000888 <kanumaiku@gmail.com>
2015-03-29 00:34:45 +0900

    update test

--- prob002/python/test.sh
+++ prob002/python/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 #input string echo passed into the test program.
-python prob002.py yolo trol > prob0001.out
+python prob002.py yolo trol > prob002.out
 echo 'here is the diff with the solution (blank means correct): '
-diff prob0001.out answer
+diff prob002.out answer>prob002.diff
+cat prob002.diff
