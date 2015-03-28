45912db9d72912cb553c7a799caed5cada891379
iku000888 <kanumaiku@gmail.com>
2015-03-29 00:37:54 +0900

    update test to show output

--- prob002/python/test.sh
+++ prob002/python/test.sh
@@ -2,6 +2,8 @@
 
 #input string echo passed into the test program.
 python prob002.py yolo trol > prob002.out
+echo 'output of program:'
+cat prob002.out
 echo 'here is the diff with the solution (blank means correct): '
 diff prob002.out answer>prob002.diff
 cat prob002.diff
