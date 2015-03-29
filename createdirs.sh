ffbd2422a0ad9eb94d1e98bf2fc10bb373a7cd5a
iku000888 <kanumaiku@gmail.com>
2015-03-29 12:56:32 +0900

    add directory structures

--- createdirs.sh
+++ createdirs.sh
@@ -1,4 +1,6 @@
 echo 'creating python and ruby sub directories for:'$1
-mkdir $1
-mkdir $1/python
-mkdir $1/ruby
+mkdir -p $1
+mkdir -p $1/python
+mkdir -p $1/ruby
+echo 'problem statement for '$1> $1/probstatement$1.txt
+echo $2>> $1/probstatement$1.txt
