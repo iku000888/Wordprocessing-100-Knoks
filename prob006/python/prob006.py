34efcbb89eca6ee8cb55971bde48e8a4b4411bf6
iku000888 <kanumaiku@gmail.com>
2015-04-02 00:08:32 +0900

    solve prob 006 with python

--- prob006/python/prob006.py
+++ prob006/python/prob006.py
@@ -3,7 +3,12 @@ import ngram
 
 bigram_x = ngram.compute_char_n_gram(sys.argv[1],2)
 bigram_y = ngram.compute_char_n_gram(sys.argv[2],2)
+qualifier = sys.argv[3]
 
-print bigram_x
-print bigram_y
-
+print "bigrams of \""+ sys.argv[1] +"\": ", bigram_x
+print "bigrams of \""+ sys.argv[2] +"\": ", bigram_y
+print "union of the bigrams:", bigram_x|bigram_y
+print "intersection of the bigrams:", bigram_x&bigram_y
+print "difference of bigrams:", bigram_x - bigram_y
+print "bigram \"" + qualifier + "\" contained in first?=>", qualifier in bigram_x
+print "bigram \"" + qualifier + "\" contained in second?=>", qualifier in bigram_y
--- prob006/python/test.sh
+++ prob006/python/test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #input string echo passed into the test program.
-python prob005.py prob005.input > prob005.out
+python prob006.py paraparaparadise paragraph se > prob006.out
 echo 'output of program:'
-cat prob005.out
+cat prob006.out
 echo 'here is the diff with the solution (blank means correct): '
-diff prob005.out answer>prob005.diff
-cat prob005.diff
+diff prob006.out answer>prob006.diff
+cat prob006.diff
