62a37655ca9e1fead4dc94d9fb43935c757f2cbf
iku000888 <kanumaiku@gmail.com>
2015-04-05 01:36:18 +0900

    add comments

--- prob006/python/prob006.py
+++ prob006/python/prob006.py
@@ -1,8 +1,15 @@
+#This program simply leverages the function ngram
+#written for the previous problem, with the built 
+#in set operation that python provides.
+
 import sys
 import ngram
 
+#obtain the bigram of param 1 and 2
 bigram_x = ngram.compute_char_n_gram(sys.argv[1],2)
 bigram_y = ngram.compute_char_n_gram(sys.argv[2],2)
+
+#string that will be checked its presence in the bi gram
 qualifier = sys.argv[3]
 
 print "bigrams of \""+ sys.argv[1] +"\": ", bigram_x
